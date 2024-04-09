8cc854a9bd182019a523b27716da4bf97dfc40b6
hanwldn76 <hanwldn76@gmail.com>
2024-04-09 16:14:52 +0900

    setting: 프로젝트 초기 설정 (#1)

---
+++ src/main/java/com/capstone/dangdang/controller/IndexController.java
@@ -0,0 +1,60 @@
+package com.capstone.dangdang.controller;
+
+import com.capstone.dangdang.entity.User;
+import com.capstone.dangdang.entity.UserRole;
+import com.capstone.dangdang.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+    @Autowired
+    private UserRepository userRepository;
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    // localhost:8080/
+    // localhost:8080
+    @GetMapping({"", "/"})
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/user")
+    public @ResponseBody String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public @ResponseBody String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/loginForm")
+    public String loginForm(){
+        return "loginForm";
+    }
+
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "joinForm";
+    }
+
+    @PostMapping("/join")
+    public String join(User user){
+        System.out.println("user = " + user);
+        user.setRole(UserRole.USER);
+
+        String rawPassword = user.getPassword();
+        String encPassword = bCryptPasswordEncoder.encode(rawPassword);
+        user.setPassword(encPassword);
+        userRepository.save(user);
+        return "redirect:/loginForm";
+    }
+}
