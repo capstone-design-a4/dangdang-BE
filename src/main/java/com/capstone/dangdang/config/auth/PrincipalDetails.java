909265a6857ca17d79fda09077edf0d31db4dc73
hanwldn76 <hanwldn76@gmail.com>
2024-04-11 00:29:47 +0900

    feature: 폼 로그인, 회원가입 기능 구현 (#5)

---
+++ src/main/java/com/capstone/dangdang/config/auth/PrincipalDetails.java
@@ -0,0 +1,59 @@
+package com.capstone.dangdang.config.auth;
+
+import com.capstone.dangdang.entity.MemberEntity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PrincipalDetails implements UserDetails {
+
+    private final MemberEntity member;
+
+    public PrincipalDetails(MemberEntity member) {
+        this.member = member;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+        collection.add(new GrantedAuthority() {
+            @Override
+            public String getAuthority() {
+                return "ROLE_" + member.getRole().name();
+            }
+        });
+
+        return collection;
+    }
+
+    @Override
+    public String getPassword() {
+        return member.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return member.getLoginId();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
